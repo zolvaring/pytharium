d70e9ebcf20e32f19bb8984f3d7083f4d9d11f82
Justin Heil <justinheil@limelin.local>
2019-03-16 12:53:40 -0700

    Create a new atom each tick

--- main.py
+++ main.py
@@ -2,28 +2,58 @@
 
 
 import time
+import uuid
+from ppretty import ppretty
 
 
 class Game():
-
   name = 'test'
 
   def __init__(self):
-
+    print('Instantiating new game')
     self.name = Game.name
+    self.things = []
 
-  @staticmethod
-  def tick():
+  #@staticmethod
+  def tick(self):
     print('Ticking game.')
+    #thing = Thing()
+    atom = Atom()
+    #print(f'Appending new thing (<{thing}>) to game things (<{self.things}>).')
+    print(f'Appending new thing (<{ppretty(atom)}>) to game things (<{ppretty(self.things)}>).')
+    self.things.append(atom)
 
-  @staticmethod
-  def loop():
+
+  #@staticmethod
+  def loop(self):
     while True:
-      Game.tick()
+      tick_time = time.time()
+      print(f'Tick time: <{ppretty(tick_time)}>')
+      self.tick()
       time.sleep(1)
     
+  
+class Thing(object):
+  def __init__(self):
+    print('Instantiating new thing')
+    super(Thing, self).__init__()
+
+    id = str(uuid.uuid4())
+    print(f'Assigning new thing instance id of <{ppretty(id)}>')
+    self.id = id
+
+    print(f'Finished building thing instance: <{ppretty(self)}>')
+
+
+class Atom(Thing):
+  def __init__(self):
+    print('Instantiating new atom')
+    super(Atom, self).__init__()
+
+    self.element = 'element_placeholder'
+
 
 if __name__ == '__main__':
-  print(f'Game name: {Game.name}.')
-  Game.loop()
-
+  print(f'Game name: <{ppretty(Game.name)}>.')
+  game = Game()
+  game.loop()
